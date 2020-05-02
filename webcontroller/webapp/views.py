b1eeab5dc157d08e0defd9db296182e54db9f8ed
ihatomorio <ihatomorio@hotmail.co.jp>
2020-05-03 03:22:08 +0900

    make list page to login requird

--- webcontroller/webapp/views.py
+++ webcontroller/webapp/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
-# from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required
 
 from .models import Sesame
 
 
-# @login_required
+@login_required
 def sesame_list(request):
-    sesames = Sesame.objects.all() #.filter(owner=request.user).order_by('-id')
+    sesames = Sesame.objects.filter(owner=request.user).order_by('-id')
     return render(request, 'sesame_list.html', {'sesames': sesames})
