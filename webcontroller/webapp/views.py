3ce34686f325324a80b25d6571f8455f9d397311
ihatomorio <ihatomorio@hotmail.co.jp>
2020-05-03 00:55:54 +0900

    Add view sesame_list

--- webcontroller/webapp/views.py
+++ webcontroller/webapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+# from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .models import Sesame
+
+
+# @login_required
+def sesame_list(request):
+    sesames = Sesame.objects.filter(owner=request.user).order_by('-id')
+    return render(request, 'sesame_list.html', {'sesames': sesames})
