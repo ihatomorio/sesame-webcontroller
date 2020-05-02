8da1aeea0e8c662dc6576acfa5b9fccf22e2e67e
ihatomorio <ihatomorio@hotmail.co.jp>
2020-05-03 00:55:00 +0900

    Add Sesame model

--- webcontroller/webapp/models.py
+++ webcontroller/webapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Sesame(models.Model):
+    def __str__(self):
+        return self.owner.__str__() + ": " + self.name.__str__()
+
+    # its owner
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    # user named name
+    name = models.CharField(max_length=256)
+
+    # auth token
+    auth_token = models.CharField(max_length=76)
