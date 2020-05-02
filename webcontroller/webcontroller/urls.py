a7df09d1e6d6997bd76ca76243ab7ab325f71dd1
ihatomorio <ihatomorio@hotmail.co.jp>
2020-05-03 03:21:10 +0900

    Add basic login page

--- webcontroller/webcontroller/urls.py
+++ webcontroller/webcontroller/urls.py
@@ -20,4 +20,5 @@ from django.urls import include
 urlpatterns = [
     path('', include('webapp.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
